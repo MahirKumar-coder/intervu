1bf772db75ca727cacba1a7281da587963a595cc
MahirKumar <mahir.kumar@s.amity.edu>
2026-08-01 07:34:53 +0530

    fix: switch to free gemini model and add partitioned cookies

--- server/src/utils/cookies.js
+++ server/src/utils/cookies.js
@@ -4,6 +4,7 @@ export const cookieOptions = {
     httpOnly: true,
     secure: isProduction,
     sameSite: isProduction ? "none" : "lax",
+    partitioned: isProduction,
     maxAge: 7 * 24 * 60 * 60 * 1000
 }
 
